e737ccee2fde62c7e1bfeeb1d8870a9fa22a735d
GabrielaFabíola <gaabstudies@gmail.com>
2022-08-17 12:36:07 -0300

    Adicionando pastas

---
+++ ModuloAPI/WebApplicationAgenda/WebApplicationAgenda/Context/AgendaContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationAgenda.Entities;
+
+namespace WebApplicationAgenda.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        { 
+
+        }
+
+        public DbSet<Contato> Contatos { get; set; }
+    }
+}
